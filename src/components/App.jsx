2661c9703477ac1ef4a86ebc8051d25ae2264d3c
Olena <newsroommy@gmail.com>
2023-01-23 17:40:22 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,56 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Component } from 'react';
+// import ContactForm from './ContactForm/ContactForm';
+import css from './ContactForm/ContactForm.module.css';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  nameInputId = nanoid();
+  // метод, який оновить state
+  handleInputChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form className="conactForm" onSubmit={this.handleSubmit}>
+          <div className={css.contactFormWrapper}>
+            <div className={css.nameLabelWrapper}>
+              <label className="" htmlFor={this.nameInputId}>
+                Name
+              </label>
+            </div>
+            <input
+              className={css.input}
+              // зв'язок інпуту і state
+              value={this.state.name}
+              onChange={this.handleInputChange}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              id={this.nameInputId}
+            />
+            <div className={css.btnAddContactWrapper}>
+              <button className={css.btnAddContact} type="submit">
+                Add contact
+              </button>
+            </div>
+          </div>
+        </form>
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,27 @@
+// import css from './ContactForm.module.css';
+// const ContactForm = () => {
+//   return (
+//     <form className="conactForm">
+//       <div className={css.contactFormWrapper}>
+//         <div className={css.nameLabelWrapper}>
+//           <label className="">Name</label>
+//         </div>
+//         <input
+//           className={css.input}
+//           // value={this.state.name}
+//           type="text"
+//           name="name"
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//           required
+//         />
+//         <div className={css.btnAddContactWrapper}>
+//           <button className={css.btnAddContact} type="submit">
+//             Add contact
+//           </button>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// };
+// export default ContactForm;
