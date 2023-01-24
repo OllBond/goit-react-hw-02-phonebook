7e2910cba37650c817f32a898441c964ed158f96
Olena <newsroommy@gmail.com>
2023-01-24 11:14:01 +0200

    update App.jsx, ContactForm.modules.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,33 +23,77 @@ export class App extends Component {
     return (
       <div>
         <h1>Phonebook</h1>
-        <form className="conactForm" onSubmit={this.handleSubmit}>
-          <div className={css.contactFormWrapper}>
-            <div className={css.nameLabelWrapper}>
-              <label className="" htmlFor={this.nameInputId}>
-                Name
-              </label>
-            </div>
-            <input
-              className={css.input}
-              // зв'язок інпуту і state
-              value={this.state.name}
-              onChange={this.handleInputChange}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              id={this.nameInputId}
-            />
-            <div className={css.btnAddContactWrapper}>
+        <div className={css.wrapper}>
+          <div className={css.contactFormBlock}>
+            <form className="" onSubmit={this.handleSubmit}>
+              <div className={css.conactFormGroup}>
+                <label className="" htmlFor={this.nameInputId}>
+                  Name
+                </label>
+                <input
+                  className={css.input}
+                  // зв'язок інпуту і state
+                  value={this.state.name}
+                  onChange={this.handleInputChange}
+                  type="text"
+                  name="name"
+                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                  required
+                  id={this.nameInputId}
+                />
+              </div>
+              <div className={css.conactFormGroup}>
+                <label className="" htmlFor={this.nameInputId}>
+                  Number
+                </label>
+                <input
+                  className={css.input}
+                  // зв'язок інпуту і state
+                  value={this.state.name}
+                  onChange={this.handleInputChange}
+                  type="text"
+                  name="name"
+                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                  required
+                  id={this.nameInputId}
+                />
+              </div>
               <button className={css.btnAddContact} type="submit">
                 Add contact
               </button>
-            </div>
+            </form>
           </div>
-        </form>
-        <h2>Contacts</h2>
+          <div className="">
+            <h2>Contacts</h2>
+            <div className={css.conactFormGroup}>
+              <label className="" htmlFor={this.nameInputId}>
+                Find contacts by name
+              </label>
+              <input
+                className={css.input}
+                // зв'язок інпуту і state
+                value={this.state.name}
+                onChange={this.handleInputChange}
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                id={this.nameInputId}
+              />
+            </div>
+            <ul className={css.listContact}>
+              <li className={css.listItems}>
+                Rosie Simpson: 459-12-56{' '}
+                <button className={css.btnDeleteContact} type="button">
+                  Delete
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
